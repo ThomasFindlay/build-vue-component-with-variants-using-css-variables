a6e6a08eedfe89a2473d187a855a304d1dba5060
Thomas <thomasfindlay>
2021-03-02 14:37:32 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :class="$style.container">
+    <BaseButton variant="primary"> Primary </BaseButton>
+    <BaseButton variant="secondary"> Secondary </BaseButton>
+    <BaseButton variant="warning"> Warning </BaseButton>
+    <BaseButton variant="danger"> Danger </BaseButton>
+  </div>
+</template>
+
+<script>
+import BaseButton from "./components/BaseButton.vue";
+export default {
+  components: {
+    BaseButton,
+  },
+};
+</script>
+
+<style module>
+.container {
+  width: 300px;
+  margin: 0 auto;
+  display: grid;
+  gap: 2rem;
+  grid-template-columns: repeat(4, 1fr);
+}
+</style>
---
+++ src/components/BaseButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <button :class="[$style.baseButton]" v-bind="$attrs">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    variant: {
+      type: String,
+      validators: value =>
+        ["primary", "secondary", "warning", "danger"].includes(value),
+    },
+  },
+};
+</script>
+
+<style module>
+.baseButton {
+  border: none;
+  background: transparent;
+  padding: 0.6rem 1rem;
+}
+
+.primary {
+}
+
+.secondary {
+}
+
+.warning {
+}
+
+.danger {
+}
+</style>
